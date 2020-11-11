9dcb448108bc7f10a07a148020eb39f92cfa2814
Jobostic <jakob-cbh@hotmail.com>
2020-11-11 16:20:40 +0100

    Final product

--- main.java
+++ main.java
@@ -2,14 +2,12 @@ import java.awt.*;
 
 public class main {
 
-    public static void main(String[] args){
-        Volvo240 vol = new Volvo240(Color.black, 100);
-
+ /*   public static void main(String[] args){
+        Volvo240 vol = new Volvo240();
+        vol.incrementSpeed(10);
         System.out.println(vol.getCurrentSpeed());
-        Saab95 sab = new Saab95(Color.red, 100);
-        sab.gas(2);
-        System.out.println();
-
+        Saab95 sab = new Saab95();
+        sab.incrementSpeed(10);
         System.out.println(sab.getCurrentSpeed());
-    }
+    }*/
 }
